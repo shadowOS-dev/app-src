90c7eb7b7f4cb96c2cb21f7ebe145697d6dab73c
Kevin Alavik <kevin@alavik.se>
2025-03-18 21:19:14 +0100

    feat/init: Made a proper clear

--- init/init.c
+++ init/init.c
@@ -3,7 +3,7 @@
 void _start(void)
 {
     // Print some fancy bytes to clear screen
-    write(STDOUT, "\033[2J\033[H\033[?25l", 12);
+    write(STDOUT, "\033[2J\033[H\033[?25l ", 13);
 
     // Print message
     write(STDOUT, "this init script currently does nothing but eh, atleast it runs :^)\n", 71);
