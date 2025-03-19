09d03af14bfba05e7d08e3cd2da9bd68af92d90b
Kevin Alavik <kevin@alavik.se>
2025-03-19 21:00:02 +0100

    fix/init: Reverted test

--- init/init.c
+++ init/init.c
@@ -3,14 +3,14 @@
 void _start(void)
 {
     // Create /test.txt
-    int fd = open("/test.txt", 0, 0); // 0x0002 -> regular file.
+    int fd = open("/test.txt", O_CREATE, 0x0002); // 0x0002 -> regular file.
     if (fd == -1)
     {
         exit(1);
     }
 
     // Write some stuff into it
-    write(fd, "Hello\n", 7);
+    write(fd, "Hello from a new file!\n", 24);
 
     // Print the contents on out to stdout
     stat_t s;
