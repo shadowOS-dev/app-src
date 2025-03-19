ec7b3021786c5634bc1f4cc75c8dbf6e2d284f1d
Kevin Alavik (Code Server) <kevin@alavik.se>
2025-03-19 07:58:55 +0100

    fix/init: Dont clean screen

--- init/init.c
+++ init/init.c
@@ -2,10 +2,7 @@
 
 void _start(void)
 {
-    // Print some fancy bytes to clear screen
-    write(STDOUT, "\033[2J\033[H\033[?25l ", 13);
-
-    // Print message
+    // todo
     write(STDOUT, "this init script currently does nothing but eh, atleast it runs :^)\n", 71);
     exit(0);
 }
