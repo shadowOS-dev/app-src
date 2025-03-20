cdbae30529ffd5be065b2d136e91cabf4e4a7267
Kevin Alavik <kevin@alavik.se>
2025-03-20 20:39:42 +0100

    fix: Dont hardcode CC and LD

--- init/build.sh
+++ init/build.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
+CC="${CC:-gcc}"
+LD="${LD:-ld}"
 cd "$(dirname "$0")" || exit 1
-gcc -ffreestanding -fno-builtin -fno-stack-protector -fno-pic -mno-red-zone -nostdlib -c syscall.c -o syscall.o
-gcc -ffreestanding -fno-builtin -fno-stack-protector -fno-pic -mno-red-zone -nostdlib -c init.c -o init.o
-ld syscall.o init.o -o init -T linker.ld -nostdlib --no-dynamic-linker --strip-all
+$CC -ffreestanding -fno-builtin -fno-stack-protector -fno-pic -mno-red-zone -nostdlib -c syscall.c -o syscall.o
+$CC -ffreestanding -fno-builtin -fno-stack-protector -fno-pic -mno-red-zone -nostdlib -c init.c -o init.o
+$LD syscall.o init.o -o init -T linker.ld -nostdlib --no-dynamic-linker --strip-all
