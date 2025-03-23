36959470a1d85b283d0d72fa1e83d2d26192f034
Kevin Alavik <kevin@alavik.se>
2025-03-23 09:30:22 +0100

    fix/init: fixed syscall shit

--- init/build.sh
+++ init/build.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 cd "$(dirname "$0")" || exit 1
+set -xe
 CC="${CC:-gcc}"
 LD="${LD:-ld}"
 $CC -ffreestanding -fno-builtin -fno-stack-protector -fno-pic -mno-red-zone -nostdlib -c init.c -o init.o
