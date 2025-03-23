d2356c08007bc5c56e801643a11d32cb835d6d88
Kevin Alavik <kevin@alavik.se>
2025-03-23 12:39:38 +0100

    feat/init: Added printf, temp

--- init/init.c
+++ init/init.c
@@ -1,7 +1,49 @@
 #include "syscall.h"
 
+#define NANOPRINTF_USE_FIELD_WIDTH_FORMAT_SPECIFIERS 1
+#define NANOPRINTF_USE_PRECISION_FORMAT_SPECIFIERS 1
+#define NANOPRINTF_USE_FLOAT_FORMAT_SPECIFIERS 0
+#define NANOPRINTF_USE_LARGE_FORMAT_SPECIFIERS 1
+#define NANOPRINTF_USE_BINARY_FORMAT_SPECIFIERS 0
+#define NANOPRINTF_USE_WRITEBACK_FORMAT_SPECIFIERS 0
+#define NANOPRINTF_SNPRINTF_SAFE_TRIM_STRING_ON_OVERFLOW 1
+
+typedef long ssize_t;
+
+#define NANOPRINTF_IMPLEMENTATION
+#include "nanoprintf.h"
+
+// Printf stuff, for debugging
+void put(const char *data, size_t length)
+{
+    write(stdout, data, length);
+}
+
+int vprintf(const char *fmt, va_list args)
+{
+    char buffer[1024];
+    int length = npf_vsnprintf(buffer, sizeof(buffer), fmt, args);
+
+    if (length >= 0 && length < (int)sizeof(buffer))
+    {
+        put(buffer, length);
+    }
+
+    return length;
+}
+
+int printf(const char *fmt, ...)
+{
+    va_list args;
+    va_start(args, fmt);
+    int length = vprintf(fmt, args);
+    va_end(args);
+    return length;
+}
+
+// Entry
 void _start(void)
 {
-    write(stdout, "Hello, World!\n", 14);
+    printf("Hello, World (using %s)!\n", "printf");
     exit(0);
 }
