4e0a00ce9fc048d89b42c33b81e76e516bc0190b
Kevin Alavik <kevin@alavik.se>
2025-03-23 09:30:58 +0100

    fix/init: Dont link with syscall.o since it doesnt exist

--- init/build.sh
+++ init/build.sh
@@ -4,4 +4,4 @@ set -xe
 CC="${CC:-gcc}"
 LD="${LD:-ld}"
 $CC -ffreestanding -fno-builtin -fno-stack-protector -fno-pic -mno-red-zone -nostdlib -c init.c -o init.o
-$LD syscall.o init.o -o init -T linker.ld -nostdlib --no-dynamic-linker --strip-all
+$LD init.o -o init -T linker.ld -nostdlib --no-dynamic-linker --strip-all
