80bbbbdb0625e621e85c3409741c1fcba752bb08
Kevin Alavik <kevin@alavik.se>
2025-05-06 21:02:42 +0200

    fix

--- init/init.c
+++ init/init.c
@@ -1,15 +1,56 @@
 #include "syscall.h"
 
+#define NANOPRINTF_USE_FIELD_WIDTH_FORMAT_SPECIFIERS 1
+#define NANOPRINTF_USE_PRECISION_FORMAT_SPECIFIERS 1
+#define NANOPRINTF_USE_FLOAT_FORMAT_SPECIFIERS 0
+#define NANOPRINTF_USE_LARGE_FORMAT_SPECIFIERS 1
+#define NANOPRINTF_USE_BINARY_FORMAT_SPECIFIERS 0
+#define NANOPRINTF_USE_WRITEBACK_FORMAT_SPECIFIERS 0
+#define NANOPRINTF_SNPRINTF_SAFE_TRIM_STRING_ON_OVERFLOW 1
+
+typedef long ssize_t;
+
+#define NANOPRINTF_IMPLEMENTATION
+#include "nanoprintf.h"
+
+int vprintf(const char *fmt, va_list args)
+{
+    char buffer[1024];
+    int length = npf_vsnprintf(buffer, sizeof(buffer), fmt, args);
+
+    if (length >= sizeof(buffer))
+    {
+        buffer[sizeof(buffer) - 1] = '\0';
+    }
+
+    if (length >= 0 && length < (int)sizeof(buffer))
+    {
+        write(0, buffer, length);
+    }
+
+    return length;
+}
+
+int printf(const char *fmt, ...)
+{
+    va_list args;
+    va_start(args, fmt);
+    int length = vprintf(fmt, args);
+    va_end(args);
+    return length;
+}
+
 void _start(void)
 {
-    write(0, "Try writing something: ", 24);
+
+    printf("[*] Try writing something: ");
     int fd = open("/dev/ps2kb", 0, 0);
     if (fd == -1)
     {
-        write(0, "Failed to open keyboard\n", 25);
+        printf("[!] Failed to open keyboard\n");
         exit(1);
     }
 
-    write(0, "keyboard is open!\n", 19);
+    printf("[+] keyboard is open!\n");
     exit(0);
 }
