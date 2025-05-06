f3254f66ada2fb005b0c9e48f6d0bdab3aef6e61
Kevin Alavik <kevin@alavik.se>
2025-05-06 21:12:45 +0200

    feat: cpuinfo

--- init/init.c
+++ init/init.c
@@ -42,18 +42,42 @@ int printf(const char *fmt, ...)
 
 void _start(void)
 {
-
-    printf("[*] Try writing something: ");
-    int fd = open("/dev/ps2kb1", 0, 0);
+    char buf[1024];
+    int fd = open("/proc/cpuinfo", 0, 0);
     if (fd == -1)
-    {
-        printf("[!] Failed to open keyboard\n");
         exit(1);
+
+    int bytes_read = read(fd, buf, sizeof(buf) - 1);
+    if (bytes_read == -1)
+        exit(1);
+
+    buf[bytes_read] = '\0';
+    close(fd);
+
+    char vendor_id_marker[] = "vendor_id";
+    int i = 0;
+    while (i < bytes_read - 9)
+    {
+        int j = 0;
+        while (j < 9 && buf[i + j] == vendor_id_marker[j])
+        {
+            j++;
+        }
+
+        if (j == 9)
+        {
+            i += 10;
+            char vendor_id[12];
+            int k = 0;
+            while (buf[i] != '\n' && k < 12 && i < bytes_read)
+            {
+                vendor_id[k++] = buf[i++];
+            }
+            vendor_id[k] = '\0';
+            write(0, vendor_id, k);
+            exit(0);
+        }
+        i++;
     }
-
-    uint8_t scancode = 0;
-    while (read(fd, &scancode, 1) != -1)
-        printf("%c", scancode);
-
-    exit(0);
+    exit(1);
 }
